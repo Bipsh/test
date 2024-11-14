cd22d093954624c43b9ebdbeecf7305329cacaaf
bipsh <a.dr.99@mail.ru>
2024-11-14 15:09:37 +0500

    cycle

--- test.js
+++ test.js
@@ -10,3 +10,12 @@ function exponent(a,b,c){
 }
 let exponentResult = exponent(oneNum, oneNum, oneNum);
 console.log(`${exponentResult} Число ${exponentResult % 2 === 0 ? 'четное' : 'нечетное'}`);
+
+let array = [];
+for (let i=0; i<6; i++){
+    const number1 = Math.floor(Math.random() * 300);
+    const number2 = Math.floor(Math.random() * 300);
+    const result = exponent(number1, number2, i);
+    console.log(`Сумма ${number1} и ${number2} в степени ${i} равна ${result}`);
+       
+}
